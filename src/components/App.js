f855125e262f1e4ecfebcf98ef90d30c50622069
VadymWebC <vadymcdevelweb@gmail.com>
2023-08-25 17:20:53 +0300

    devel

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
 import React from 'react'
+import { useState } from 'react'
 import './App.css'
 
 const App = ({ grid }) => {
+    const [version, setVersion] = useState(0)
+
     return (
         <div>
             <div>Click on the red cells</div>
             {grid.map((row, i) => (
                 <div key={i}>
                     {row.map((val, j) => (
-                        <div key={i * row.length + j} className="cell"></div>
+                        <div
+                            key={i * row.length + j}
+                            className={`cell ${
+                                val ? 'cell-red' : 'cell-green'
+                            }`}
+                            onClick={() => {
+                                if (val === 1) {
+                                    dfs(grid, i, j)
+                                }
+                            }}
+                        ></div>
                     ))}
                 </div>
             ))}
